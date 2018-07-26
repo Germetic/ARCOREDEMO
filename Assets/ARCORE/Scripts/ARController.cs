6b43faf50660381ec3e92e39d125baf2e19de361
DESKTOP-FPREBHK\germe <unity.amconsoft1@gmail.com>
2018-07-26 14:55:44 +0300

    DemoProject v1

---
+++ Assets/ARCORE/Scripts/ARController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+#if UNITY_EDITOR
+using input = GoogleARCore.InstantPreviewInput;
+#endif
+public class ARController : MonoBehaviour {
+
+    private List<TrackedPlane> m_NewTracedPlanes = new List<TrackedPlane>();
+
+    public GameObject GridPrefab;
+    public GameObject Portal;
+    public GameObject ARCamera;
+
+    private void Update()
+    {
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+        Session.GetTrackables<TrackedPlane>(m_NewTracedPlanes, TrackableQueryFilter.New);
+
+        for (int i = 0; i < m_NewTracedPlanes.Count; i++)
+        {
+            GameObject grid = Instantiate(GridPrefab,Vector3.zero,Quaternion.identity,transform);
+            grid.GetComponent<GridVisualizer>().Initialize(m_NewTracedPlanes[i]);
+        }
+
+        Touch touch;
+        if(Input.touchCount <1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)           
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        if(Frame.Raycast(touch.position.x,touch.position.y,TrackableHitFlags.PlaneWithinPolygon,out hit))
+        {
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+            Portal.SetActive(true);
+
+            Portal.transform.position = hit.Pose.position;
+            Portal.transform.rotation = hit.Pose.rotation;
+
+            Vector3 cameraPosition = ARCamera.transform.position;
+            cameraPosition.y = hit.Pose.position.y;
+            Portal.transform.LookAt(cameraPosition,Portal.transform.up);
+
+            Portal.transform.parent = anchor.transform;
+        }
+        
+    }
+}
---
+++ Assets/PortalManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalManager : MonoBehaviour
+{
+
+    public GameObject MainCamera;
+    public GameObject Sponza;
+    private List<Material> SponzaMaterials;
+    private void Start()
+    {
+        SponzaMaterials = new List<Material>();
+
+        // SponzaMaterials = Sponza.GetComponent<Renderer>().sharedMaterials;
+        Renderer[] childRends = Sponza.GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < childRends.Length; i++)
+        {
+            SponzaMaterials.Add(childRends[i].sharedMaterial);
+        }
+        SetShaderVisible(false);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "MainCamera") return;
+        SetShaderVisible(true);
+
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag != "MainCamera") return;
+        SetShaderVisible(false);
+    }
+    private void SetShaderVisible(bool isVisible)
+    {
+        if (isVisible)
+        {
+            for (int i = 0; i < SponzaMaterials.Count; i++)
+            {
+                SponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Always);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < SponzaMaterials.Count; i++)
+            {
+                SponzaMaterials[i].SetInt("_StencilComp", (int)CompareFunction.Equal);
+            }
+        }
+    }
+}
